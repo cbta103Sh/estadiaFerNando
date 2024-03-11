9b07d032b4b6853dcef3de43a3b0ad46920f6465
cbta103Sh <saturninohernan053@gmail.com>
2024-03-11 09:47:52 -0600

    El segundo commit de mi estadia

--- public/js/script.js
+++ public/js/script.js
@@ -11,26 +11,23 @@ async function login() {
             body: JSON.stringify({ userName: username, pass: password })
         });
 
-        // Verifica el tipo de contenido de la respuesta
+        if (!response.ok) {
+            throw new Error('Error al iniciar sesión: ' + response.statusText);
+        }
+
         const contentType = response.headers.get('content-type');
         if (contentType && contentType.includes('application/json')) {
             const data = await response.json();
 
-            if (response.status === 200) {
-                // Redirige según el tipo de usuario
-                if (data.message === 'Administrador') {
-                    window.location.href = '/addReport.html'; 
-                } else if (data.message === 'Prefecto') {
-                    window.location.href = '/prefectosAdd.html'; 
-                } else {
-                    document.getElementById('message').innerText = 'Acceso no autorizado';
-                }
+            if (data.message === 'Administrador') {
+                window.location.href = '/addReport.html'; 
+            } else if (data.message === 'Prefecto') {
+                window.location.href = '/prefectosAdd.html'; 
             } else {
-                document.getElementById('message').innerText = 'Credenciales incorrectas';
+                document.getElementById('message').innerText = 'Acceso no autorizado';
             }
         } else {
-            // Si no es JSON, muestra un mensaje de error
-            document.getElementById('message').innerText = 'Respuesta no válida del servidor';
+            throw new Error('Respuesta no válida del servidor');
         }
     } catch (error) {
         console.error('Error al iniciar sesión:', error);
