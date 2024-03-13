57e3046da1ce905accbb9f0c7be1d48bea362e6a
cbta103Sh <saturninohernan053@gmail.com>
2024-03-12 20:04:33 -0600

    modificacion de la liga de mongodb

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 // db.js
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/estadia', {
+mongoose.connect('mongodb+srv://SturninoHernan:cbta103SH@fernando.n4wjqef.mongodb.net/', {
  
 });
 
